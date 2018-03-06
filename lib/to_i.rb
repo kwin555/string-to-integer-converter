e91aef2050d0294c84274417c879f141bb313a87
kwin555 <ktn555@att.net>
2018-03-06 11:30:22 -0600

    refractored code and added a check for negatiave numbers

--- lib/to_i.rb
+++ lib/to_i.rb
@@ -1,13 +1,29 @@
 # creates a to integer method that converts a string to an integer
 module ToInteger
+  def self.negative?(num_array)
+    num_array.include?(45)
+  end
+
+  def self.negatve_remover(num_array)
+    num_array.delete_if { |num| num == 45 }
+  end
+
   def self.string_to_integer_array(string)
     string.split('').map do |num|
-      num.ord - 48
+      num.ord
+    end
+  end
+  def self.conversion_to_decimal_system(num_array)
+    num_array.map do |num|
+      num - 48
     end
   end
 
   def self.to_integer(string_input)
     num_array = string_to_integer_array(string_input)
+    negative_check = negative?(num_array)
+    num_array = negatve_remover(num_array)
+    num_array = conversion_to_decimal_system(num_array)
     power = num_array.size - 1
     i = 0
     while i <= num_array.size - 1
@@ -15,6 +31,7 @@ module ToInteger
       i += 1
       power -= 1
     end
-    num_array.reduce(:+)
+    negative_check ? num_array.reduce(:+) * - 1 : num_array.reduce(:+)
   end
 end
+puts ToInteger.to_integer('34343')
--- spec/to_i_spec.rb
+++ spec/to_i_spec.rb
@@ -6,9 +6,19 @@ RSpec.describe ToInteger, type: :model do
       expect(ToInteger.to_integer('34234')).to eq 34_234
     end
   end
+  describe '.to_integer' do
+    it 'should return the interger version of the string input' do
+      expect(ToInteger.to_integer('-90')).to eq -90
+    end
+  end
+  describe '.is_negative?' do
+    it 'checks for ascii value of 45 which is a negative sign' do
+      expect(ToInteger.negative?([45])).to eq true
+    end
+  end
   describe '.string_to_integer_array' do
     it 'should return the interger version of the string input' do
-      expect(ToInteger.string_to_integer_array('34234')).to eq [3, 4, 2, 3, 4]
+      expect(ToInteger.string_to_integer_array('34234')).to eq [51, 52, 50, 51, 52]
     end
   end
 end
