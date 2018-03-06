9af006bfed82208745b9b138393626a301ac83a2
kwin555 <ktn555@att.net>
2018-03-06 12:31:33 -0600

    refractored code, used a map hash table instead of ascii values

--- lib/to_i.rb
+++ lib/to_i.rb
@@ -1,29 +1,20 @@
 # creates a to integer method that converts a string to an integer
 module ToInteger
   def self.negative?(num_array)
-    num_array.include?(45)
+    num_array.include?(nil)
   end
 
   def self.negatve_remover(num_array)
-    num_array.delete_if { |num| num == 45 }
+    num_array.delete_if { |num| num == nil }
   end
 
-  def self.string_to_integer_array(string)
+  def self.string_to_char_array(string)
     string.split('').map do |num|
-      num.ord
-    end
-  end
-  def self.conversion_to_decimal_system(num_array)
-    num_array.map do |num|
-      num - 48
+      num
     end
   end
 
-  def self.to_integer(string_input)
-    num_array = string_to_integer_array(string_input)
-    negative_check = negative?(num_array)
-    num_array = negatve_remover(num_array)
-    num_array = conversion_to_decimal_system(num_array)
+  def self.array_of_integers_into_integer(num_array)
     power = num_array.size - 1
     i = 0
     while i <= num_array.size - 1
@@ -31,7 +22,36 @@ module ToInteger
       i += 1
       power -= 1
     end
-    negative_check ? num_array.reduce(:+) * - 1 : num_array.reduce(:+)
+    num_array.reduce(:+)
+  end
+
+  def self.char_to_integer_array(char_array)
+    nums = []
+    map = {
+      '0' => 0,
+      '1' => 1,
+      '2' => 2,
+      '3' => 3,
+      '4' => 4,
+      '5' => 5,
+      '6' => 6,
+      '7' => 7,
+      '8' => 8,
+      '9' => 9
+    }
+    char_array.each do |char|
+      num = map[char]
+      nums << num
+    end
+    nums
+  end
+
+  def self.to_integer(string_input)
+    char_array = string_to_char_array(string_input)
+    num_array = char_to_integer_array(char_array)
+    negative_check = negative?(num_array)
+    negative_check ? negatve_remover(num_array) : num_array
+    integer = array_of_integers_into_integer(num_array)
+    negative_check ? integer * - 1 : integer
   end
 end
-puts ToInteger.to_integer('34343')
--- spec/to_i_spec.rb
+++ spec/to_i_spec.rb
@@ -1,24 +1,39 @@
 require 'spec_helper'
 
 RSpec.describe ToInteger, type: :model do
-  describe '.to_integer' do
-    it 'should return the interger version of the string input' do
-      expect(ToInteger.to_integer('34234')).to eq 34_234
+  describe '.string_to_char_array' do
+    it 'should split the string input to an array of chars' do
+      expect(ToInteger.string_to_char_array('34234')).to eq ['3', '4', '2', '3', '4']
+    end
+  end
+  describe '.char_to_integer_array' do
+    it 'should should convert the array of chars in to an array of integers' do
+      expect(ToInteger.char_to_integer_array(['3', '4', '2', '3', '4'])).to eq [3, 4, 2, 3, 4]
+    end
+  end
+  describe '.array_of_integers_into_integer' do
+    it 'should should convert the array of chars in to an array of integers' do
+      expect(ToInteger.array_of_integers_into_integer([3, 4, 2, 3, 4])).to eq 34_234
+    end
+  end
+  describe '.negative?' do
+    it 'finds a nil in the array of integers' do
+      expect(ToInteger.negative?([nil, 4, 2, 3, 4])).to eq true
+    end
+  end
+  describe '.negatve_remover' do
+    it 'removes the nil from the array of integers' do
+      expect(ToInteger.negatve_remover([nil, 4, 2, 3, 4])).to eq [4, 2, 3, 4]
     end
   end
   describe '.to_integer' do
-    it 'should return the interger version of the string input' do
-      expect(ToInteger.to_integer('-90')).to eq -90
+    it 'takes a string and converts to integer (positive)' do
+      expect(ToInteger.to_integer('34242')).to eq 34_242
     end
   end
-  describe '.is_negative?' do
-    it 'checks for ascii value of 45 which is a negative sign' do
-      expect(ToInteger.negative?([45])).to eq true
-    end
-  end
-  describe '.string_to_integer_array' do
-    it 'should return the interger version of the string input' do
-      expect(ToInteger.string_to_integer_array('34234')).to eq [51, 52, 50, 51, 52]
+  describe '.to_integer' do
+    it 'takes a string and converts to integer (positive)' do
+      expect(ToInteger.to_integer('-34242')).to eq (-34_242)
     end
   end
 end
